5e43cd779a6270531610413d8c64cc91f0a6ea34
ikhwanizh <ikhwaninzaghi999@gmail.com>
2023-08-28 14:48:24 +0700

    init

---
+++ app/src/main/java/com/ikhwanizh/androidtutorial/ui/RelativeLayout.kt
@@ -0,0 +1,12 @@
+package com.ikhwanizh.androidtutorial.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ikhwanizh.androidtutorial.R
+
+class RelativeLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative_layout)
+    }
+}
